931232c8301a957a9323ab305f7f405f8e35d657
Oct <rcabrera@outlook.com>
2023-06-23 15:20:35 -0400

    Taskcreator started

--- frontend/src/components/ButtonComponent.js
+++ frontend/src/components/ButtonComponent.js
@@ -1,13 +1,13 @@
 import React, {useState} from 'react'
 
 function ButtonComponent({buttonClass, buttonText, onClick}) {
-    const [active, setactive] = useState(false)
+    const [active, setActive] = useState(false)
     const getChevronClassName = () => {
         if (active) return "chevron-arrow-right";
         else return "";
     }
     return ( 
-    <div className={buttonClass} onClick={onClick} onMouseOver={()=>setactive(true)} onMouseOut={()=>setactive(false)}> 
+    <div className={buttonClass} onClick={onClick} onMouseOver={()=>setActive(true)} onMouseOut={()=>setActive(false)}> 
     <h3> {buttonText} </h3>
     <div className={getChevronClassName()}/>
     </div>
--- frontend/src/pages/TaskViewerUser.js
+++ frontend/src/pages/TaskViewerUser.js
@@ -1,18 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState , useContext} from 'react'
 import ButtonComponent from '../components/ButtonComponent'
 import Popup from '../components/Popup'
+import AuthContext from '../context/AuthProvider';
+
+
 
 
 
 
   
-function TaskViewerUser() {
+function TaskViewer() {
+
+  const { auth } = useContext(AuthContext);
+  
   const [buttonPopup, setButtonPopup] = useState(false);
 
+  
   const [data, setData] = useState([
     { model: 22, colour: "black", size: "large", quantity: 2, description:"smooth", task: "cutting", progress: 0 },
     { model: 22, colour: "black", size: "large", quantity: 2, description:"smooth", task: "cutting", progress: 0 }
   ])
+  
+
   
   const saveProgress = (quantity,progress,key) =>{
   if (progress < quantity ) {
@@ -26,6 +35,8 @@ function TaskViewerUser() {
     updatedData[key].progress = parseInt(e.target.value);
     setData(updatedData);
   };
+
+  if (auth.admin===false) {
   return (
     
     <div className='taskviewer-container'>
@@ -77,8 +88,39 @@ function TaskViewerUser() {
     </div >
     
     
-  )
+  )}
+  else {return (
+    <div className='taskviewer-container'>
+
+        <h2> TaskViewer </h2>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Model</th>
+                <th>Colour</th>
+                <th>Size</th>
+                <th>Quantity</th>
+                <th>Task</th>
+                <th>Progress</th>
+            </tr>
+            {data.map((val, key) => {
+                return (
+                    <tr key={key}>
+                        <td>{val.name}</td>
+                        <td>{val.model}</td>
+                        <td>{val.colour}</td>
+                        <td>{val.size}</td>
+                        <td>{val.quantity}</td>
+                        <td>{val.task}</td>
+                        <td>{val.progress}</td>
+                    </tr>
+                )
+            })}
+        </table>
+        <ButtonComponent buttonClass="page-switch-button" onClick={() => setCreatorPage(true)} buttonText="Task Creator"/>
+    </div >
+)}
 }
 // onClick={"function for axios post"} <button className="progress-button" onClick={() => setButtonPopup(true)}>Edit Progress</button>
 //<ButtonComponent buttonClass="progress-button" onClick={() => setButtonPopup(true)} buttonText="Edit Progress"/>
-export default TaskViewerUser
+export default TaskViewer
