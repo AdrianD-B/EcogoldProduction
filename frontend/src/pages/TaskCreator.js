e22aeaeffc46fc64f57f2fbfc8c7350a2572d1c4
Oct <rcabrera@outlook.com>
2023-06-28 15:59:46 -0400

    Linked Task Viewer and Creator pages, enabled task creation, small changes

--- frontend/src/pages/TaskCreator.js
+++ frontend/src/pages/TaskCreator.js
@@ -1,49 +1,123 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ButtonComponent from '../components/ButtonComponent'
 import FormInput from '../components/FormInput';
+import axios from "axios"
 
-const data = [
-  { name: "Adrian", model: 22, colour: "black", size: "large", quantity: 2, task: "cutting", progress: "0" },
-  { name: "Roméo", model: 25, colour: "blue", size: "XL", quantity: 1, task: "packing", progress: "5/12" }
-]
 
-function TaskCreator() {
+function TaskCreator({ setCreatorPage }) {
+  const [formDetails, setFormDetails] = useState([])
+  const [users, setUsers] = useState([{_id: 0, name:"test"}])
 
+  const handleFormInputChange = (e) => {
+    setFormDetails({
+      ...formDetails,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleTaskCreation = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.post("http://localhost:3001/api/task/create", formDetails,
+        {
+          headers: { 'Content-Type': 'application/json' },
+          withCredentials: false
+        });
+      console.log(response)
+    } catch (error) {
+      console.log(error.response.data)
+    }
+  }
+  const getUsers = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/api/task/getUsers",
+        {
+          headers: { 'Content-Type': 'application/json' },
+          withCredentials: false
+        });
+      setUsers(response.data)
+      console.log(response.data)
+    } catch (error) {
+      console.log(error.response.data)
+    }
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
 
   return (
-    <div className='taskviewer-container'>
+    <div className='taskcreator-container'>
 
       <h2> TaskCreator </h2>
-      <table>
-        <tr>
-          <th>Name</th>
-          <th>Model</th>
-          <th>Colour</th>
-          <th>Size</th>
-          <th>Quantity</th>
-          <th>Task</th>
-          <th>Progress</th>
-        </tr>
-        {data.map((val, key) => {
-          return (
-            <tr key={key}>
-              <td><input
-                name="name"
-                value={val.progress}
-                type="string"
-                placeholder='0'
-                
-              /></td>
-              <td>{val.model}</td>
-              <td>{val.colour}</td>
-              <td>{val.size}</td>
-              <td>{val.quantity}</td>
-              <td>{val.task}</td>
-              <td>{val.progress}</td>
-            </tr>
-          )
-        })}
-      </table>
+      <form className="task-creation-form" onSubmit={handleTaskCreation}>
+        <div>
+        <input className="login-form-item" list="userlist" name="name" onChange={handleFormInputChange} />
+        
+          {users.length > 0 && (
+          <datalist id="userlist">
+          {users.map(user => (
+            <option key={user._id} value={user.name} />
+            
+          ))}
+
+
+        </datalist>)}
+            
+        <FormInput
+          inputClass="login-form-item"
+          value={formDetails.model}
+          onChange={handleFormInputChange}
+          inputType="string"
+          header="Model"
+        />
+        <FormInput
+          inputClass="login-form-item"
+          value={formDetails.description}
+          onChange={handleFormInputChange}
+          inputType="string"
+          header="Description"
+        />
+        <FormInput
+          inputClass="login-form-item"
+          value={formDetails.color}
+          onChange={handleFormInputChange}
+          inputType="string"
+          header="Color"
+        />
+        </div>
+        <div>
+        <FormInput
+          inputClass="login-form-item"
+          value={formDetails.size}
+          onChange={handleFormInputChange}
+          inputType="string"
+          header="Size"
+        />
+        <FormInput
+          inputClass="login-form-item"
+          value={formDetails.quantity}
+          onChange={handleFormInputChange}
+          inputType="number"
+          header="Quantity"
+        />
+
+        <input className="login-form-item" list="tasks" name="task" onChange={handleFormInputChange} />
+        <datalist id="tasks">
+          <option value="Cutting" />
+          <option value="Sewing" />
+          <option value="Prep" />
+
+        </datalist>
+
+
+        <ButtonComponent buttonClass="page-switch-button" onClick={() => setCreatorPage(false)} buttonText="Task Viewer" />
+        <button type="submit" className="create-task-button-container"><ButtonComponent buttonClass="create-task-button" buttonText="Create Task" /></button>
+        </div>
+       
+      </form>
+      
+      
     </div >
 
   )
